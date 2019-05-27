ad5167370413d7ca17b9c2a99cc417bfa2075ee8
Alexander Jipa <alexander.jipa@gmail.com>
2019-05-26 21:22:12 -0700

    problem 654

---
+++ src/main/java/problems600_699/Problem654.java
@@ -0,0 +1,41 @@
+package problems600_699;
+
+import lombok.Data;
+
+public class Problem654 {
+
+    @Data
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public static TreeNode constructMaximumBinaryTree(int[] nums) {
+        return constructMaximumBinaryTree(nums, 0, nums.length - 1);
+    }
+
+    private static TreeNode constructMaximumBinaryTree(int[] nums, int left, int right) {
+        if (right - left <= 0) {
+            return new TreeNode(nums[left]);
+        }
+        int pos = left;
+        int max = nums[left];
+        for (int i = left + 1; i <= right; i++) {
+            int cur = nums[i];
+            if (max < cur) {
+                max = cur;
+                pos = i;
+            }
+        }
+        TreeNode result = new TreeNode(max);
+        if (pos - left > 0) {
+            result.left = constructMaximumBinaryTree(nums, left, pos - 1);
+        }
+        if (right - pos > 0) {
+            result.right = constructMaximumBinaryTree(nums, pos + 1, right);
+        }
+        return result;
+    }
+}
