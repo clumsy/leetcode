d8d3c7c0d683386fcd2e037b631ee5db249673f4
Alexander Jipa <alexander.jipa@gmail.com>
2020-01-06 22:37:44 -0800

    simplifying problem 21

--- src/main/java/leetcode/Problem21.java
+++ src/main/java/leetcode/Problem21.java
@@ -1,11 +1,9 @@
 package leetcode;
 
-import java.util.StringJoiner;
-
 public class Problem21 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode root = null;
-        ListNode current = null;
+        ListNode root = new ListNode(0);
+        ListNode current = root;
         while (l1 != null) {
             ListNode next;
             if (l2 == null || l1.val <= l2.val) {
@@ -15,25 +13,14 @@ public class Problem21 {
                 next = new ListNode(l2.val);
                 l2 = l2.next;
             }
-            if (root == null) {
-                root = next;
-                current = root;
-            } else {
-                current.next = next;
-                current = current.next;
-            }
+            current.next = next;
+            current = current.next;
         }
         while (l2 != null) {
-            ListNode next = new ListNode(l2.val);
-            if (root == null) {
-                root = next;
-                current = root;
-            } else {
-                current.next = next;
-                current = current.next;
-            }
+            current.next = new ListNode(l2.val);
+            current = current.next;
             l2 = l2.next;
         }
-        return root;
+        return root.next;
     }
 }
