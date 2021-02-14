f9123903ac63170f0868c51d2323def0d9cf35cd
Alexander Jipa <alexander.jipa@gmail.com>
2021-02-14 00:31:31 -0500

    optimized problem 15

--- src/main/java/leetcode/Problem15.java
+++ src/main/java/leetcode/Problem15.java
@@ -1,35 +1,34 @@
 package leetcode;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Problem15 {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
-        for (int i = 0, count = nums.length - 2; i < count; i++) {
-            int x = nums[i];
-            if (i > 0 && x == nums[i - 1]) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            if (x > 0) {
+            if (nums[i] > 0) { // because numbers are sorted, no solution when first number is positive
                 break;
             }
             int l = i + 1;
             int r = nums.length - 1;
             while (l < r) {
-                int y = nums[l];
-                int z = nums[r];
-                int sum = x + y + z;
-                if (sum == 0) {
-                    result.add(Arrays.asList(x, y, z));
-                    while (l < r && nums[l + 1] == y) {
-                        l++;
-                    }
+                if (l > i + 1 && nums[l] == nums[l - 1]) {
                     l++;
-                    while (l < r && nums[r - 1] == z) {
-                        r--;
-                    }
+                    continue;
+                }
+                if (r < nums.length - 1 && nums[r] == nums[r + 1]) {
                     r--;
+                    continue;
+                }
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[l++], nums[r]));
                 } else if (sum < 0) {
                     l++;
                 } else {
