82fbc06f9d2aacd1cc0a534ea9c642f42966c19c
Alexander Jipa <alexander.jipa@gmail.com>
2021-02-15 21:37:14 -0500

    optimized problem 33

--- src/main/java/leetcode/Problem33.java
+++ src/main/java/leetcode/Problem33.java
@@ -2,41 +2,27 @@ package leetcode;
 
 public class Problem33 {
     public int search(int[] nums, int target) {
-        if (nums.length == 0) {
-            return -1;
-        }
-        return doSearch(target, nums, 0, nums.length - 1);
-    }
-
-    private int doSearch(int target, int[] nums, int lo, int hi) {
-        int left = nums[lo];
-        if (target == left) {
-            return lo;
-        }
-        int right = nums[hi];
-        if (target == right) {
-            return hi;
-        }
-        if (hi - lo < 2) {
-            return -1;
-        }
-        int mid = (hi + lo + 1)/2;
-        int middle = nums[mid];
-        if (target == middle) {
-            return mid;
-        }
-        if (hi - lo < 3) {
-            return -1;
-        }
-        if (middle > left) {
-            if (target < middle && target > left) {
-                return doSearch(target, nums, lo + 1, mid - 1);
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = (lo + hi)/2;
+            if (target == nums[mid]) {
+                return mid;
             }
-            return doSearch(target, nums, mid + 1, hi - 1);
+            if (nums[lo] <= nums[mid]) { // left is sorted
+                if (target < nums[mid] && target >= nums[lo]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
         }
-        if (target > middle && target < right) {
-            return doSearch(target, nums, mid + 1, hi - 1);
-        }
-        return doSearch(target, nums, lo + 1, mid - 1);
+        return -1;
     }
 }
