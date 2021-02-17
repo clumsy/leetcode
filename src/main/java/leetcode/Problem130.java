06a663a44dbfd31ff02b94fc627f46a9a1f7e549
Alexander Jipa <alexander.jipa@gmail.com>
2021-02-17 01:01:59 -0500

    optimized problem 130

--- src/main/java/leetcode/Problem130.java
+++ src/main/java/leetcode/Problem130.java
@@ -1,83 +1,46 @@
 package leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
-
 class Problem130 {
     public void solve(char[][] board) {
-        UnionFind uf = new UnionFind(board.length*board[0].length);
         int rows = board.length;
+        if (rows == 0) {
+            return;
+        }
         int cols = board[0].length;
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                if (board[row][col] == 'X') {
-                    continue;
-                }
-                if (row < rows - 1 && board[row + 1][col] == 'O') {
-                    uf.union(row*cols + col, (row + 1)*cols + col);
-                }
-                if (col < cols - 1 && board[row][col + 1] == 'O') {
-                    uf.union(row*cols + col, row*cols + col + 1);
-                }
-                if (row > 0 && board[row - 1][col] == 'O') {
-                    uf.union(row*cols + col, (row - 1)*cols + col);
-                }
-                if (col > 0 && board[row][col - 1] == 'O') {
-                    uf.union(row*cols + col, row*cols + col - 1);
-                }
-            }
+        if (cols == 0) {
+            return;
         }
 
-        Set<Integer> remains = new HashSet<>();
         for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                if (row == 0 || col == 0 || row == rows - 1 || col == cols - 1) {
-                    remains.add(row*cols + col);
-                }
-            }
+            dfs(board, row, 0);
+            dfs(board, row, cols - 1);
+        }
+
+        for (int col = 0; col < cols; col++) {
+            dfs(board, 0, col);
+            dfs(board, rows - 1, col);
         }
 
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
-                if (board[row][col] == 'O' && !remains.contains(row*cols + col)) {
+                if (board[row][col] != '*') {
                     board[row][col] = 'X';
+                } else {
+                    board[row][col] = 'O';
                 }
             }
         }
     }
 
-    private class UnionFind {
-        private int[] parent;
-        private int[] size;
-
-        private UnionFind(int total) {
-            this.parent = new int[total];
-            this.size = new int[total];
-            for (int i = 0; i < total; i++) {
-                parent[i] = i;
-                size[i] = 1;
-            }
+    private void dfs(char[][] board, int row, int col) {
+        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] != 'O') {
+            return;
         }
-
-        int find(int p) {
-            if (p != parent[p]) { // path compression
-                parent[p] = find(parent[p]);
-                return parent[p];
-            }
-            return p;
-        }
-
-        void union(int p, int q) {
-            int componentP = find(p);
-            int componentQ = find(q);
-            if (size[componentP] > size[componentQ]) {
-                parent[componentQ] = componentP;
-                size[componentP] += size[componentQ];
-            } else {
-                parent[componentP] = componentQ;
-                size[componentQ] += size[componentP];
-            }
-        }
+        board[row][col] = '*';
+        dfs(board, row - 1, col);
+        dfs(board, row + 1, col);
+        dfs(board, row, col - 1);
+        dfs(board, row, col + 1);
     }
 }
 
