0ed1471d055d24f72eefd6c43a8b5e28aebcf8ea
Alexander Jipa <alexander.jipa@gmail.com>
2021-02-19 00:22:19 -0500

    problem 50

---
+++ src/main/java/leetcode/Problem50.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class Problem50 {
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return x;
+        }
+        if (n == 2) {
+            return x*x;
+        }
+        if (n == 3) {
+            return x*x*x;
+        }
+        if (n < 0 && n > Integer.MIN_VALUE) {
+            return myPow(1/x, -n);
+        }
+        double fourth = myPow(x*x, n/4);
+        return myPow(x, n%4)*fourth*fourth;
+    }
+}
---
+++ src/test/java/leetcode/Problem50Test.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class Problem50Test {
+    private final Problem50 solution = new Problem50();
+
+    @Test
+    public void example1() {
+        assertThat(solution.myPow(2, 10), is(1024));
+    }
+
+    @Test
+    public void example2() {
+        assertThat(solution.myPow(2.1, 3), is(9.261));
+    }
+}
