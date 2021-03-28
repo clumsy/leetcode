9fba7b7b65835f106f0e4515f9b788dd072aca99
Alexander Jipa <alexander.jipa@gmail.com>
2021-03-28 00:34:08 -0400

    problem 398 improved

--- src/main/java/leetcode/Problem398.java
+++ src/main/java/leetcode/Problem398.java
@@ -2,29 +2,30 @@ package leetcode;
 
 import java.util.*;
 
-@BeatsPercent(55.73)
+@BeatsPercent(60.59)
 @TimeComplexity(Complexity.LINEAR_N)
 @SpaceComplexity(Complexity.LINEAR_N)
 public class Problem398 {
     public class Solution {
         private final Random random = new Random();
-        private final Map<Integer, List<Integer>> numToIndices = new HashMap<>();
+        private final int[] nums;
 
         public Solution(int[] nums) {
-            for (int i = 0; i < nums.length; i++) {
-                List<Integer> indices = numToIndices.get(nums[i]);
-                if (indices == null) {
-                    indices = new ArrayList<>();
-                    numToIndices.put(nums[i], indices);
-                }
-                indices.add(i);
-            }
+            this.nums = nums;
         }
 
         public int pick(int target) {
-            List<Integer> indices = numToIndices.get(target);
-            int nextIndex = random.nextInt(indices.size());
-            return indices.get(nextIndex);
+            int index = 0;
+            int count = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == target) {
+                    count++;
+                    if (random.nextInt(count) == 0) {
+                        index = i;
+                    }
+                }
+            }
+            return index;
         }
     }
 }
