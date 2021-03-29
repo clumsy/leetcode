605323a7a47612ec627dd04425e6309281b48e82
Alexander Jipa <alexander.jipa@gmail.com>
2021-03-28 22:43:34 -0400

    problem 1029 improved

--- src/main/java/leetcode/Problem1029.java
+++ src/main/java/leetcode/Problem1029.java
@@ -1,14 +1,25 @@
 package leetcode;
 
-import java.util.Arrays;
-
-@BeatsPercent(37.74)
-@TimeComplexity(Complexity.LINEARITHMIC_N)
+@BeatsPercent(100)
+@TimeComplexity(Complexity.LINEAR_N)
 @SpaceComplexity(Complexity.CONSTANT)
 public class Problem1029 {
     public int twoCitySchedCost(int[][] costs) {
-        Arrays.sort(costs, (a, b) -> (b[1] - b[0]) - (a[1] - a[0]));
+        // skipping array shuffle due to the small size (2 to 100)
         int n = costs.length/2;
+        int start = 0;
+        int end = costs.length - 1;
+        while (true) {
+            int position = partition(costs, start, end);
+            if (position == n) {
+                break;
+            }
+            if (position > n) {
+                end = position - 1;
+            } else {
+                start = position + 1;
+            }
+        }
         int min = 0;
         for (int i = 0; i < costs.length; i++) {
             if (i >= n) {
@@ -18,5 +29,28 @@ public class Problem1029 {
             }
         }
         return min;
+    }
+
+    private int compare(int[][] costs, int i, int j) {
+        return (costs[j][1] - costs[j][0]) - (costs[i][1] - costs[i][0]);
+    }
+
+    private void swap(int[][] costs, int i, int j) {
+        int[] tmp = costs[i];
+        costs[i] = costs[j];
+        costs[j] = tmp;
+    }
+
+    private int partition(int[][] costs, int start, int end) {
+        int middle = start + (end - start)/2;
+        swap(costs, middle, end);
+        int j = start;
+        for (int i = start; i < end; i++) {
+            if (compare(costs, i, end) < 0) {
+                swap(costs, i, j++);
+            }
+        }
+        swap(costs, j, end);
+        return j;
     }
 }
--- src/test/java/leetcode/Problem1029Test.java
+++ src/test/java/leetcode/Problem1029Test.java
@@ -25,4 +25,18 @@ public class Problem1029Test {
         int[][] costs = {{515,563},{451,713},{537,709},{343,819},{855,779},{457,60},{650,359},{631,42}};
         assertThat(solution.twoCitySchedCost(costs), is(3086));
     }
+    
+    @Test
+    public void example4() {
+        int[][] costs = {{440,704},{481,140},{810,854},{34,150},{684,360},{516,301},{790,678},
+            {138,893},{899,260},{881,499},{500,572},{419,657},{179,234},{965,136},{826,886},
+            {880,913},{362,934},{747,219},{676,17},{847,713}};
+        assertThat(solution.twoCitySchedCost(costs), is(7911));
+    }
+    
+    @Test
+    public void example5() {
+        int[][] costs = {{10,10},{30,30},{400,400},{30,30}};
+        assertThat(solution.twoCitySchedCost(costs), is(470));
+    }
 }
