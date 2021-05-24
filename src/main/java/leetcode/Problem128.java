aac3b2142a4a85b6adfe59eced51d1ed2af18f86
Alexander Jipa <alexander.jipa@gmail.com>
2021-05-24 00:17:00 -0400

    problem 128

--- src/main/java/leetcode/Problem128.java
+++ src/main/java/leetcode/Problem128.java
@@ -3,65 +3,30 @@ package leetcode;
 import java.util.HashMap;
 import java.util.Map;
 
-@BeatsPercent(35.07)
+@BeatsPercent(40.01)
 @TimeComplexity(Complexity.LINEAR_N)
 @SpaceComplexity(Complexity.LINEAR_N)
 public class Problem128 {
     public int longestConsecutive(int[] nums) {
-        UnionFind uf = new UnionFind(1001);
-        for (int num : nums) {
-            uf.add(num);
-        }
-        return uf.max_consecutive_length;
-    }
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>(1001, 1);
+        for (int n : nums) {
+            if (!map.containsKey(n)) {
+                int left = map.getOrDefault(n - 1, 0);
+                int right = map.getOrDefault(n + 1, 0);
 
-    class UnionFind {
-        private final Map<Integer, Integer> parent;
-        private final Map<Integer, Integer> size;
-        private int max_consecutive_length;
+                // sum: length of the sequence n is in
+                int sum = left + right + 1;
+                map.put(n, sum);
 
-        private UnionFind(int n) {
-            this.parent = new HashMap<>(n, 1);
-            this.size = new HashMap<>(n, 1);
-        }
+                // keep track of the max length 
+                answer = Math.max(answer, sum);
 
-        private void add(int x) {
-            if (parent.containsKey(x)) {
-                return;
-            }
-            parent.put(x, x);
-            size.put(x, 1);
-            max_consecutive_length = Math.max(max_consecutive_length, 1);
-            if (parent.containsKey(x - 1)) {
-                union(x, x - 1);
-            }
-            if (parent.containsKey(x + 1)) {
-                union(x, x + 1);
+                // extend the length to the boundary(s) of the sequence  will do nothing if n has no neighbors
+                map.put(n - left, sum);
+                map.put(n + right, sum);
             }
         }
-
-        private int find(int x) {
-            int p = parent.get(x);
-            if (p != x) {
-                parent.put(x, find(p));
-            }
-            return parent.get(x);
-        }
-
-        private void union(int x, int y) {
-            int p_x = find(x);
-            int p_y = find(y);
-            if (p_x != p_y) {
-                if (size.get(p_x) >= size.get(p_y)) {
-                    parent.put(p_y, p_x);
-                    size.put(p_x, size.get(p_x) + size.get(p_y));
-                    max_consecutive_length = Math.max(max_consecutive_length, size.get(p_x));
-                } else {
-                    parent.put(p_x, p_y);
-                    size.put(p_y, size.get(p_y) + size.get(p_x));
-                    max_consecutive_length = Math.max(max_consecutive_length, size.get(p_y));
-                }
-            }
-        }
+        return answer;
     }
 }
