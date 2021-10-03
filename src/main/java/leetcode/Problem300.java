2e966267588e1badc4d7da8476ba7ea7c9ab511b
Alexander Jipa <alexander.jipa@gmail.com>
2021-10-03 09:21:17 -0400

    updating problem 300

--- src/main/java/leetcode/Problem300.java
+++ src/main/java/leetcode/Problem300.java
@@ -1,46 +1,28 @@
 package leetcode;
 
-@BeatsPercent(89.36)
-@Algorithms(Algorithm.FENWICK_TREE)
+@BeatsPercent(99.45)
+@Algorithms(Algorithm.PATIENCE_SORTING)
 @TimeComplexity(worst = Complexity.LINEARITHMIC_N)
 @SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem300 {
-    static class Fenwick {
-        private final int[] tree;
-
-        Fenwick(int size) {
-            this.tree = new int[size + 1];
-        }
-
-        void update(int pos, int val) {
-            pos++;
-            while (pos < tree.length) {
-                tree[pos] = Math.max(tree[pos], val);
-                pos += pos & -pos;
-            }
-        }
-
-        int query(int pos) {
-            pos++;
-            int val = 0;
-            while (pos > 0) {
-                val = Math.max(tree[pos], val);
-                pos -= pos & -pos;
-            }
-            return val;
-        }
-    }
-
     public int lengthOfLIS(int[] nums) {
-        Fenwick bit = new Fenwick(20001); // [-10000;10000]
-        int ans = 0;
-        for (int num : nums) {
-            int i = num + 10000;
-            int current = bit.query(i - 1);
-            current++;
-            bit.update(i, current);
-            ans = Math.max(ans, current);
+        int piles = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int lo = 0;
+            int hi = piles;
+            while (lo < hi) {
+                int mid = lo + (hi - lo)/2;
+                if (nums[mid] < nums[i]) {
+                    lo = mid + 1; // finding lower-bound
+                } else {
+                    hi = mid;
+                }
+            }
+            nums[hi] = nums[i];
+            if (hi == piles) { // new pile
+                ++piles;
+            }
         }
-        return ans;
+        return piles;
     }
 }
