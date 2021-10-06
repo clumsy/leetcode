734bc5088c632b9908d4eaab272c322bc91ffe65
Alexander Jipa <alexander.jipa@gmail.com>
2021-10-05 22:59:22 -0400

    adding difficulty annotation

---
+++ src/main/java/leetcode/Difficulty.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+
+@Target(TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Difficulty {
+    Level value();
+}
+
+enum Level {
+    EASY,
+    MEDIUM,
+    HARD;
+}
