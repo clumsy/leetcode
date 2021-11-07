ef131d7dfdfe1f854359f6ef1547fa7bf1309b00
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-07 00:05:44 -0400

    updating problem 190

--- src/main/java/leetcode/Problem190.java
+++ src/main/java/leetcode/Problem190.java
@@ -1,17 +1,22 @@
 package leetcode;
 
+@Difficulty(Level.EASY)
+@Algorithms(Algorithm.BIT_MANIPULATION)
 @BeatsPercent(100)
+@TimeComplexity(worst = Complexity.LINEAR_N) // where N is the number of 1 bits
+@SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem190 {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        int result = 0;
-        int k = 0;
-        while (k < 32) {
-            if ((n & (1 << k)) != 0) {
-                result |= 1 << (31 - k);
+        int answer = 0;
+        int mask = 1 << 31;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                answer |= mask;
             }
-            k++;
+            mask >>>= 1;
+            n >>>= 1;
         }
-        return result;
+        return answer;
     }
 }
--- src/test/java/leetcode/Problem190Test.java
+++ src/test/java/leetcode/Problem190Test.java
@@ -1,17 +1,19 @@
 package leetcode;
 
-import static org.junit.Assert.assertThat;
-
 import org.junit.Test;
-
-import static org.hamcrest.core.Is.is;
 
 public class Problem190Test {
     private final Problem190 solution = new Problem190();
 
     @Test
     public void example1() {
-        int n = Integer.parseInt("00000010100101000001111010011100", 2);
-        assertThat(solution.reverseBits(n), is(964176192));
+        int n = 0b00000010100101000001111010011100;
+        assert solution.reverseBits(n) == 0b00111001011110000010100101000000;
+    }
+
+    @Test
+    public void example2() {
+        int n = 0b11111111111111111111111111111101;
+        assert solution.reverseBits(n) == 0b10111111111111111111111111111111;
     }
 }
