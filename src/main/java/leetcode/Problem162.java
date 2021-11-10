b59a010c3dc842a4decef302e55ac66c69b9c369
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-09 23:07:33 -0500

    updating problem 162

--- src/main/java/leetcode/Problem162.java
+++ src/main/java/leetcode/Problem162.java
@@ -1,19 +1,22 @@
 package leetcode;
 
+@Difficulty(Level.MEDIUM)
+@Algorithms(Algorithm.BINARY_SEARCH)
 @BeatsPercent(100)
+@TimeComplexity(worst = Complexity.LOGARITHMIC_N)
+@SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem162 {
     public int findPeakElement(int[] nums) {
-        int N = nums.length;
-        int left = 0;
-        int right = N - 1;
-        while (left < right) {
-            int middle = left + (right - left)/2;
-            if (nums[middle] < nums[middle + 1]) {
-                left = middle + 1;
-            } else {
-                right = middle;
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (hi - lo > 1) {
+            int mid = lo + (hi - lo)/2;
+            if (nums[mid] < nums[mid + 1]) { // increases - there should be a peak on the right
+                lo = mid + 1;
+            } else { // decreases - there should be a peak on the left
+                hi = mid;
             }
         }
-        return left;
+        return nums[lo] < nums[hi] ? hi : lo;
     }
 }
--- src/test/java/leetcode/Problem162Test.java
+++ src/test/java/leetcode/Problem162Test.java
@@ -2,19 +2,18 @@ package leetcode;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
 public class Problem162Test {
     private final Problem162 solution = new Problem162();
 
     @Test
     public void example1() {
-        assertThat(solution.findPeakElement(new int[] {1,2,3,1}), is(2));
+        int[] nums = {1, 2, 3, 1};
+        assert solution.findPeakElement(nums) == 2;
     }
 
     @Test
     public void example2() {
-        assertThat(solution.findPeakElement(new int[] {1,2,1,3,5,6,4}), is(5));
+        int[] nums = {1, 2, 1, 3, 5, 6, 4};
+        assert solution.findPeakElement(nums) == 5;
     }
 }
