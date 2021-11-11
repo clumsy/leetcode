beb801d268919a97b79540c4bd396f71e4e315ea
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-10 21:29:30 -0500

    problem 82

--- src/main/java/leetcode/Problem82.java
+++ src/main/java/leetcode/Problem82.java
@@ -1,11 +1,15 @@
 package leetcode;
 
+@Difficulty(Level.MEDIUM)
+@Algorithms(Algorithm.TWO_POINTERS)
 @BeatsPercent(100)
+@TimeComplexity(worst = Complexity.LINEAR_N)
+@SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem82 {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode newHead = new ListNode(0);
-        newHead.next = head;
-        ListNode previous = newHead;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode previous = dummy;
         while (head != null) {
             if (head.next != null && head.val == head.next.val) {
                 while (head.next != null && head.val == head.next.val) {
@@ -17,6 +21,6 @@ public class Problem82 {
             }
             head = head.next;
         }
-        return newHead.next;
+        return dummy.next;
     }
 }
--- src/test/java/leetcode/Problem82Test.java
+++ src/test/java/leetcode/Problem82Test.java
@@ -1,9 +1,8 @@
 package leetcode;
 
-import static org.junit.Assert.assertThat;
-
-import static org.hamcrest.core.Is.is;
 import org.junit.Test;
+
+import java.util.Objects;
 
 public class Problem82Test {
     private final Problem82 solution = new Problem82();
@@ -11,12 +10,12 @@ public class Problem82Test {
     @Test
     public void example1() {
         ListNode list = ListNodes.list(1,2,3,3,4,4,5);
-        assertThat(solution.deleteDuplicates(list), is(ListNodes.list(1,2,5)));
+        assert Objects.equals(solution.deleteDuplicates(list), ListNodes.list(1,2,5));
     }
 
     @Test
     public void example2() {
         ListNode list = ListNodes.list(1,1,1,2,3);
-        assertThat(solution.deleteDuplicates(list), is(ListNodes.list(2,3)));
+        assert Objects.equals(solution.deleteDuplicates(list), ListNodes.list(2,3));
     }
 }
