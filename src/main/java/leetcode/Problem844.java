96f2bcb89a0aa50fb5936447a90f25b645bd1c0b
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-11 23:12:59 -0500

    updating problem 844

--- src/main/java/leetcode/Problem844.java
+++ src/main/java/leetcode/Problem844.java
@@ -1,45 +1,31 @@
 package leetcode;
 
+@Difficulty(Level.EASY)
+@Algorithms(Algorithm.TWO_POINTERS)
 @BeatsPercent(100)
-@TimeComplexity(Complexity.LINEAR_N) // where N = |s| + |t|
-@SpaceComplexity(Complexity.CONSTANT)
+@TimeComplexity(worst = Complexity.N_PLUS_M)
+@SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem844 {
     public boolean backspaceCompare(String s, String t) {
         int i = s.length() - 1;
         int j = t.length() - 1;
+        int removes;
         while (true) {
-            int k = 0;
-            while (i >= 0 && s.charAt(i) == '#') {
-                while (i >= 0 && s.charAt(i) == '#') {
-                    i--;
-                    k++;
-                }
-                while (k > 0 && i >= 0 && s.charAt(i) != '#') {
-                    k--;
-                    i--;
-                }
-            }
-            
-            int p = 0;
-            while (j >= 0 && t.charAt(j) == '#') {
-                while (j >= 0 && t.charAt(j) == '#') {
-                    j--;
-                    p++;
-                }
-                while (p > 0 && j >= 0 && t.charAt(j) != '#') {
-                    p--;
-                    j--;
-                }
+            removes = 0;
+            while (i >= 0 && (removes > 0 || s.charAt(i) == '#')) {
+                removes += s.charAt(i--) == '#' ? 1 : -1;
             }
 
-            if (i < 0 && j < 0) {
-                return true;
+            removes = 0;
+            while (j >= 0 && (removes > 0 || t.charAt(j) == '#')) {
+                removes += t.charAt(j--) == '#' ? 1 : -1;
             }
-            if (i < 0 || j < 0) {
-                return false;
-            }
-            if (s.charAt(i--) != t.charAt(j--)) {
-                return false;
+
+            if (i >= 0 && j >= 0 && s.charAt(i) == t.charAt(j)) {
+                i--;
+                j--;
+            } else {
+                return i == -1 && j == - 1;
             }
         }
     }
--- src/test/java/leetcode/Problem844Test.java
+++ src/test/java/leetcode/Problem844Test.java
@@ -2,39 +2,36 @@ package leetcode;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
 public class Problem844Test {
     private final Problem844 solution = new Problem844();
     
     @Test
     public void example1() {
-        assertThat(solution.backspaceCompare("ab#c", "ad#c"), is(true));
+        assert solution.backspaceCompare("ab#c", "ad#c");
     }
     
     @Test
     public void example2() {
-        assertThat(solution.backspaceCompare("ab##", "c#d#"), is(true));
+        assert solution.backspaceCompare("ab##", "c#d#");
     }
     
     @Test
     public void example3() {
-        assertThat(solution.backspaceCompare("a##c", "#a#c"), is(true));
+        assert solution.backspaceCompare("a##c", "#a#c");
     }
     
     @Test
     public void example4() {
-        assertThat(solution.backspaceCompare("a#c", "b"), is(false));
+        assert !solution.backspaceCompare("a#c", "b");
     }
     
     @Test
     public void example5() {
-        assertThat(solution.backspaceCompare("bxj##tw", "bxo#j##tw"), is(true));
+        assert solution.backspaceCompare("bxj##tw", "bxo#j##tw");
     }
     
     @Test
     public void example6() {
-        assertThat(solution.backspaceCompare("bbbextm", "bbb#extm"), is(false));
+        assert !solution.backspaceCompare("bbbextm", "bbb#extm");
     }
 }
