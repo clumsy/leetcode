ef0cc8da3bbf004fc7c686dae3ab43ccc29f389f
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-12 21:15:45 -0500

    updating problem 21

--- src/main/java/leetcode/Problem21.java
+++ src/main/java/leetcode/Problem21.java
@@ -7,18 +7,18 @@ package leetcode;
 @SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem21 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+        while (l1 != null || l2 != null) {
+            if (l2 == null || (l1 != null && l1.val <= l2.val)) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
         }
-        if (l2 == null) {
-            return l1;
-        }
-        if (l1.val <= l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
-        } else {
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
-        }
+        return dummy.next;
     }
 }
