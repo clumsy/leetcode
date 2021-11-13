db4bec0a5850ad2fae104676e7510f5df08faaa8
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-12 22:58:46 -0500

    updating problem 209

--- src/main/java/leetcode/Problem209.java
+++ src/main/java/leetcode/Problem209.java
@@ -1,23 +1,22 @@
 package leetcode;
 
-@BeatsPercent(99.17)
+@Difficulty(Level.MEDIUM)
+@Algorithms(Algorithm.TWO_POINTERS)
+@BeatsPercent(99)
 @TimeComplexity(worst = Complexity.LINEAR_N)
 @SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem209 {
     public int minSubArrayLen(int target, int[] nums) {
-        int answer = Integer.MAX_VALUE;
-        int n = nums.length;
-        int sum = 0;
-        int start = 0;
-        for (int i = 0; i < n; i++) {
-            if (sum < target) {
-                sum += nums[i];
+        int result = Integer.MAX_VALUE;
+        for (int s = 0, e = 0, sum = 0; e < nums.length; e++) {
+            sum += nums[e];
+            while (sum - nums[s] >= target) {
+                sum -= nums[s++];
             }
-            while (sum >= target) {
-                answer = Math.min(answer, i + 1 - start);
-                sum -= nums[start++];
+            if (sum >= target) {
+                result = Math.min(result, e - s + 1);
             }
         }
-        return answer == Integer.MAX_VALUE ? 0 : answer;
+        return result == Integer.MAX_VALUE ? 0 : result;
     }
 }
--- src/test/java/leetcode/Problem209Test.java
+++ src/test/java/leetcode/Problem209Test.java
@@ -2,24 +2,24 @@ package leetcode;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
 public class Problem209Test {
     private final Problem209 solution = new Problem209();
     
     @Test
     public void example1() {
-        assertThat(solution.minSubArrayLen(7, new int[] {2,3,1,2,4,3}), is(2));
+        int[] nums = {2,3,1,2,4,3};
+        assert solution.minSubArrayLen(7, nums) == 2;
     }
     
     @Test
     public void example2() {
-        assertThat(solution.minSubArrayLen(4, new int[] {1,4,4}), is(1));
+        int[] nums = {1,4,4};
+        assert solution.minSubArrayLen(4, nums) == 1;
     }
     
     @Test
-    public void example11() {
-        assertThat(solution.minSubArrayLen(11, new int[] {1,1,1,1,1,1,1,1}), is(0));
+    public void example3() {
+        int[] nums = {1,1,1,1,1,1,1,1};
+        assert solution.minSubArrayLen(11, nums) == 0;
     }
 }
