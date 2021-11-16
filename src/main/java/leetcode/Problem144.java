09b599ef73547e0e3cf5f8ea8ecc7737d22a21c9
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-15 22:06:25 -0500

    updating problem 144

--- src/main/java/leetcode/Problem144.java
+++ src/main/java/leetcode/Problem144.java
@@ -1,49 +1,23 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Stack;
 
+@Difficulty(Level.EASY)
+@Algorithms(Algorithm.DEPTH_FIRST_SEARCH)
+@BeatsPercent(100)
+@TimeComplexity(worst = Complexity.LINEAR_N)
+@SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem144 {
-    public static class Alternative extends Problem144 {
-
-        @Override
-        public List<Integer> preorderTraversal(TreeNode root) {
-            if (root == null) {
-                return Collections.emptyList();
-            }
-            List<Integer> result = new ArrayList<>();
-            Stack<TreeNode> stack = new Stack<>();
-            stack.add(root);
-            while (!stack.isEmpty()) {
-                TreeNode current = stack.pop();
-                result.add(current.val);
-                if (current.right != null) {
-                    stack.add(current.right);
-                }
-                if (current.left != null) {
-                    stack.add(current.left);
-                }
-            }
-            return result;
-        }
-    }
-
     public List<Integer> preorderTraversal(TreeNode root) {
-        return doPreorderTraversal(root, new ArrayList<>());
+        return dfs(new ArrayList<>(), root);
     }
 
-    private static List<Integer> doPreorderTraversal(TreeNode current, List<Integer> result) {
-        if (current == null) {
-            return result;
-        }
-        result.add(current.val);
-        if (current.left != null) {
-            doPreorderTraversal(current.left, result);
-        }
-        if (current.right != null) {
-            doPreorderTraversal(current.right, result);
+    private List<Integer> dfs(List<Integer> result, TreeNode root) {
+        if (root != null) {
+            result.add(root.val);
+            dfs(result, root.left);
+            dfs(result, root.right);
         }
         return result;
     }
--- src/test/java/leetcode/Problem144Test.java
+++ src/test/java/leetcode/Problem144Test.java
@@ -2,33 +2,30 @@ package leetcode;
 
 import org.junit.Test;
 
-import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
-import static leetcode.TreeNodes.treeOf;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Problem144Test {
-    protected Problem144 solution = new Problem144();
+    private final Problem144 solution = new Problem144();
 
-    public static final class AlternativeTest extends Problem144Test {
-        {
-            this.solution = new Problem144.Alternative();
-        }
+    @Test
+    public void example1() {
+        TreeNode root = TreeNodes.treeOf();
+        assert Objects.equals(solution.preorderTraversal(root),
+            Arrays.asList());
     }
 
     @Test
-    public void empty() {
-        assertThat(solution.preorderTraversal(null), is(emptyList()));
+    public void example2() {
+        TreeNode root = TreeNodes.treeOf(1,null,2,3);
+        assert Objects.equals(solution.preorderTraversal(root),
+            Arrays.asList(1,2,3));
     }
 
     @Test
-    public void simple() {
-        assertThat(solution.preorderTraversal(treeOf(1, null, 2, 3)), is(asList(1, 2, 3)));
-    }
-
-    @Test
-    public void easy() {
-        assertThat(solution.preorderTraversal(treeOf(3, 1, 2)), is(asList(3, 1, 2)));
+    public void example3() {
+        TreeNode root = TreeNodes.treeOf(3,1,2);
+        assert Objects.equals(solution.preorderTraversal(root),
+            Arrays.asList(3,1,2));
     }
 }
