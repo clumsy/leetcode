96dd02f02cdb4befbe327a6f77c7eb0e1c88472c
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-17 00:16:31 -0500

    updating problem 90

--- src/main/java/leetcode/Problem90.java
+++ src/main/java/leetcode/Problem90.java
@@ -4,25 +4,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@BeatsPercent(100)
+@Difficulty(Level.MEDIUM)
+@Algorithms(Algorithm.BACKTRACKING)
+@BeatsPercent(99.72)
+@TimeComplexity(worst = Complexity.EXPONENTIAL_N)
+@SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem90 {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>((int) Math.pow(2, nums.length));
-        List<Integer> current = new ArrayList<>(nums.length);
-        collectSubsets(result, current, nums, 0);
-        return result;
-    }
-
-    private void collectSubsets(List<List<Integer>> result, List<Integer> current, int[] nums, int offset) {
-        result.add(new ArrayList<>(current));
-        for (int i = offset; i < nums.length; i++) {
-            if (i > offset && nums[i] == nums[i - 1]) {
-                continue;
+        List<List<Integer>> result = new ArrayList<>(1 << nums.length);
+        result.add(new ArrayList<>(nums.length));
+        int size = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int start = i > 0 && nums[i] == nums[i - 1] ? /* previous */ size : 0;
+            size = result.size();
+            for (int j = start; j < size; j++) {
+                result.add(new ArrayList<>(result.get(j)));
+                result.get(result.size() - 1).add(nums[i]);
             }
-            current.add(nums[i]);
-            collectSubsets(result, current, nums, i + 1);
-            current.remove(current.size() - 1);
         }
+        return result;
     }
 }
--- src/test/java/leetcode/Problem90Test.java
+++ src/test/java/leetcode/Problem90Test.java
@@ -3,23 +3,21 @@ package leetcode;
 import org.junit.Test;
 
 import java.util.Arrays;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import java.util.Objects;
 
 public class Problem90Test {
     private final Problem90 solution = new Problem90();
 
     @Test
     public void example1() {
-        assertThat(solution.subsetsWithDup(new int[] {1,2,2}),
-            is(
-                Arrays.asList(
-                    Arrays.asList(),
-                    Arrays.asList(1),
-                    Arrays.asList(1,2),
-                    Arrays.asList(1,2,2),
-                    Arrays.asList(2),
-                    Arrays.asList(2,2))));
+        int[] nums = {1,2,2};
+        assert Objects.equals(solution.subsetsWithDup(nums),
+            Arrays.asList(
+                Arrays.asList(),
+                Arrays.asList(1),
+                Arrays.asList(2),
+                Arrays.asList(1,2),
+                Arrays.asList(2,2),
+                Arrays.asList(1,2,2)));
     }
 }
