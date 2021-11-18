f2c948421815a265f84e3f157dfa46d06e404b6f
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-17 21:26:37 -0500

    updating problem 112

--- src/main/java/leetcode/Problem112.java
+++ src/main/java/leetcode/Problem112.java
@@ -1,13 +1,18 @@
 package leetcode;
 
+@Difficulty(Level.EASY)
+@Algorithms(Algorithm.RECURSION)
+@BeatsPercent(100)
+@TimeComplexity(worst = Complexity.LINEAR_N)
+@SpaceComplexity(worst = Complexity.LINEAR_N) // depth of the stack
 public class Problem112 {
-    public boolean hasPathSum(TreeNode root, int sum) {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) {
             return false;
         }
-        if (root.left == null && root.right == null && root.val == sum) {
-            return true;
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
         }
-        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
     }
 }
--- src/test/java/leetcode/Problem112Test.java
+++ src/test/java/leetcode/Problem112Test.java
@@ -2,15 +2,24 @@ package leetcode;
 
 import org.junit.Test;
 
-import static leetcode.TreeNodes.treeOf;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
 public class Problem112Test {
-    private Problem112 solution = new Problem112();
+    private final Problem112 solution = new Problem112();
 
     @Test
-    public void simple() {
-        assertThat(solution.hasPathSum(treeOf(5, 4, 8, 11, null, 13, 4, 7, 2, null, 1), 22), is(true));
+    public void example1() {
+        TreeNode root = TreeNodes.treeOf(5,4,8,11,null,13,4,7,2,null,1);
+        assert solution.hasPathSum(root, 22);
+    }
+
+    @Test
+    public void example2() {
+        TreeNode root = TreeNodes.treeOf(1,2,3);
+        assert !solution.hasPathSum(root, 5);
+    }
+
+    @Test
+    public void example3() {
+        TreeNode root = TreeNodes.treeOf(1,2);
+        assert !solution.hasPathSum(root, 0);
     }
 }
