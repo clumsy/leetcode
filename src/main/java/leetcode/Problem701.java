3eda0a0276d314508dc66f2e353bf3354ecffc9a
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-18 22:16:36 -0500

    updating problem 701

--- src/main/java/leetcode/Problem701.java
+++ src/main/java/leetcode/Problem701.java
@@ -1,20 +1,30 @@
 package leetcode;
 
+@Difficulty(Level.EASY)
+@Algorithms(Algorithm.RECURSION)
+@BeatsPercent(100)
+@TimeComplexity(worst = Complexity.LOGARITHMIC_N) // where N is the number of elements in BST
+@SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem701 {
-    public static TreeNode insertIntoBST(TreeNode root, int val) {
-        if (val > root.val) {
-            if (root.right == null) {
-                root.right = new TreeNode(val);
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        TreeNode current = root;
+        while (true) {
+            if (current.val < val) {
+                if (current.right == null) {
+                    current.right = new TreeNode(val);
+                    return root;
+                }
+                current = current.right;
             } else {
-                insertIntoBST(root.right, val);
-            }
-        } else {
-            if (root.left == null) {
-                root.left = new TreeNode(val);
-            } else {
-                insertIntoBST(root.left, val);
+                if (current.left == null) {
+                    current.left = new TreeNode(val);
+                    return root;
+                }
+                current = current.left;
             }
         }
-        return root;
     }
 }
--- src/test/java/leetcode/Problem701Test.java
+++ src/test/java/leetcode/Problem701Test.java
@@ -2,16 +2,36 @@ package leetcode;
 
 import org.junit.Test;
 
-import static leetcode.Problem701.insertIntoBST;
-import static leetcode.TreeNodes.bstOf;
-import static leetcode.TreeNodes.treeOf;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import java.util.Objects;
 
 public class Problem701Test {
+    private final Problem701 solution = new Problem701();
 
     @Test
-    public void simple() {
-        assertThat(insertIntoBST(bstOf(4, 2, 7, 1, 3), 5), is(bstOf(4, 2, 7, 5, 1, 3)));
+    public void example1() {
+        TreeNode root = TreeNodes.treeOf(4,2,7,1,3);
+        assert Objects.equals(solution.insertIntoBST(root, 5),
+            TreeNodes.treeOf(4,2,7,5,1,3));
+    }
+
+    @Test
+    public void example2() {
+        TreeNode root = TreeNodes.treeOf(40,20,60,10,30,50,70);
+        assert Objects.equals(solution.insertIntoBST(root, 25),
+            TreeNodes.treeOf(40,20,60,10,30,50,70,null,null,25));
+    }
+
+    @Test
+    public void example3() {
+        TreeNode root = TreeNodes.treeOf(4,2,7,1,3,null,null,null,null,null,null);
+        assert Objects.equals(solution.insertIntoBST(root, 5),
+            TreeNodes.treeOf(4,2,7,1,3,5));
+    }
+
+    @Test
+    public void example4() {
+        TreeNode root = TreeNodes.treeOf();
+        assert Objects.equals(solution.insertIntoBST(root, 5),
+            TreeNodes.treeOf(5));
     }
 }
