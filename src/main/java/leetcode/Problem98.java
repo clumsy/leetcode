64433208d0bb9001bd406a7732f7265567925526
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-19 22:59:10 -0500

    updating problem 98

--- src/main/java/leetcode/Problem98.java
+++ src/main/java/leetcode/Problem98.java
@@ -1,24 +1,37 @@
 package leetcode;
 
+@Difficulty(Level.MEDIUM)
+@Algorithms(Algorithm.RECURSION)
+@BeatsPercent(37.30)
+@TimeComplexity(worst = Complexity.LINEAR_N)
+@SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem98 {
     public boolean isValidBST(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        return isValidBST(root, null, null);
-    }
-
-    private boolean isValidBST(TreeNode root, Integer leftBound, Integer rightBound) {
-        if ((rightBound != null && root.val >= rightBound) || (leftBound != null && root.val <= leftBound)) {
-            return false;
-        }
-        if (root.left != null
-                && !isValidBST(root.left, leftBound, rightBound != null ? Math.min(rightBound, root.val) : root.val)) {
-            return false;
-        }
-        if (root.right != null
-                && !isValidBST(root.right, leftBound != null ? Math.max(leftBound, root.val) : root.val, rightBound)) {
-            return false;
+        TreeNode prev = null;
+        while (root != null) {
+            if (root.left == null) {
+                if (prev != null && prev.val >= root.val) {
+                    return false;
+                }
+                prev = root;
+                root = root.right;
+            } else {
+                TreeNode predecessor = root.left;
+                while (predecessor.right != null && predecessor.right != root) {
+                    predecessor = predecessor.right;
+                }
+                if (predecessor.right == null) {
+                    predecessor.right = root;
+                    root = root.left;
+                } else {
+                    predecessor.right = null;
+                    if (prev != null && prev.val >= root.val) {
+                        return false;
+                    }
+                    prev = root;
+                    root = root.right;
+                }
+            }
         }
         return true;
     }
--- src/test/java/leetcode/Problem98Test.java
+++ src/test/java/leetcode/Problem98Test.java
@@ -2,51 +2,55 @@ package leetcode;
 
 import org.junit.Test;
 
-import static leetcode.TreeNodes.treeOf;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
 public class Problem98Test {
-    private Problem98 solution = new Problem98();
+    private final Problem98 solution = new Problem98();
 
     @Test
-    public void valid() {
-        assertThat(solution.isValidBST(treeOf(2, 1, 3)), is(true));
+    public void example1() {
+        TreeNode root = TreeNodes.treeOf(2,1,3);
+        assert solution.isValidBST(root);
     }
 
     @Test
-    public void invalid() {
-        assertThat(solution.isValidBST(treeOf(5, 1, 4, null, null, 3, 6)), is(false));
+    public void example2() {
+        TreeNode root = TreeNodes.treeOf(5,1,4,null,null,3,6);
+        assert !solution.isValidBST(root);
     }
 
     @Test
-    public void invalidSameValue() {
-        assertThat(solution.isValidBST(treeOf(1, 1, null)), is(false));
+    public void example3() {
+        TreeNode root = TreeNodes.treeOf(1,1,null);
+        assert !solution.isValidBST(root);
     }
 
     @Test
-    public void complex() {
-        assertThat(solution.isValidBST(treeOf(10, 5, 15, null, null, 6, 20)), is(false));
+    public void example4() {
+        TreeNode root = TreeNodes.treeOf(10,5,15,null,null,6,20);
+        assert !solution.isValidBST(root);
     }
 
     @Test
-    public void empty() {
-        assertThat(solution.isValidBST(null), is(true));
+    public void example5() {
+        TreeNode root = TreeNodes.treeOf();
+        assert solution.isValidBST(root);
     }
 
     @Test
-    public void weird() {
-        assertThat(solution.isValidBST(treeOf(3, 1, 5, 0, 2, 4, 6)), is(true));
+    public void example6() {
+        TreeNode root = TreeNodes.treeOf(3, 1, 5, 0, 2, 4, 6);
+        assert solution.isValidBST(root);
     }
 
     @Test
-    public void max() {
-        assertThat(solution.isValidBST(treeOf(Integer.MAX_VALUE)), is(true));
+    public void example7() {
+        TreeNode root = TreeNodes.treeOf(Integer.MAX_VALUE);
+        assert solution.isValidBST(root);
     }
 
     @Test
-    public void huge() {
-        assertThat(solution.isValidBST(treeOf(3, 1, 5, 0, 2, 4, 6, null, null, null, 3)), is(false));
+    public void example8() {
+        TreeNode root = TreeNodes.treeOf(3,1,5,0,2,4,6,null,null,null,3);
+        assert !solution.isValidBST(root);
     }
 
 }
