3c509ba7364bf271c3214247e5bb2d63e54271ed
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-19 23:19:57 -0500

    updating problem 783

--- src/main/java/leetcode/Problem783.java
+++ src/main/java/leetcode/Problem783.java
@@ -1,36 +1,36 @@
 package leetcode;
 
+@Difficulty(Level.EASY)
 @Algorithms(Algorithm.BINARY_TREE_MORRIS_TRAVERSAL)
 @BeatsPercent(100)
-@TimeComplexity(Complexity.LINEAR_N)
-@SpaceComplexity(Complexity.CONSTANT)
+@TimeComplexity(worst = Complexity.LINEAR_N)
+@SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem783 {
     public int minDiffInBST(TreeNode root) {
         int min = Integer.MAX_VALUE;
-        TreeNode tourist = root;
         TreeNode prev = null;
-        while (tourist != null) {
-            if (tourist.left == null) {
+        while (root != null) {
+            if (root.left == null) {
                 if (prev != null) {
-                    min = Math.min(min, Math.abs(tourist.val - prev.val));
+                    min = Math.min(min, Math.abs(root.val - prev.val));
                 }
-                prev = tourist;
-                tourist = tourist.right;
+                prev = root;
+                root = root.right;
             } else {
-                TreeNode guide = tourist.left;
-                while (guide.right != null && guide.right != tourist) {
-                    guide = guide.right;
+                TreeNode predecessor = root.left;
+                while (predecessor.right != null && predecessor.right != root) {
+                    predecessor = predecessor.right;
                 }
-                if (guide.right == null) {
-                    guide.right = tourist;
-                    tourist = tourist.left;
+                if (predecessor.right == null) {
+                    predecessor.right = root;
+                    root = root.left;
                 } else {
-                    guide.right = null;
+                    predecessor.right = null;
                     if (prev != null) {
-                        min = Math.min(min, Math.abs(tourist.val - prev.val));
+                        min = Math.min(min, Math.abs(root.val - prev.val));
                     }
-                    prev = tourist;
-                    tourist = tourist.right;
+                    prev = root;
+                    root = root.right;
                 }
             }
         }
