6149cdd84f73d53ed52919d0ac42480186ca6d5c
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-21 23:40:32 -0500

    updating problem 56

--- src/main/java/leetcode/Problem56.java
+++ src/main/java/leetcode/Problem56.java
@@ -1,52 +1,31 @@
 package leetcode;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
+@Difficulty(Level.MEDIUM)
+@Algorithms(Algorithm.SORTING)
+@BeatsPercent(29.44)
+@TimeComplexity(worst = Complexity.LINEARITHMIC_N)
+@SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem56 {
-    public static final class Alternative extends Problem56 {
-
-        @Override
-        public int[][] merge(int[][] intervals) {
-            if (intervals == null || intervals.length == 0) {
-                return intervals;
-            }
-            List<int[]> sorted = Arrays.asList(intervals);
-            sorted.sort(Comparator.comparingInt(i -> i[0]));
-            List<int[]> result = new ArrayList<>(sorted.size());
-            for (int[] interval : sorted) {
-                if (result.isEmpty()) {
-                    result.add(interval);
-                    continue;
-                }
-                int[] last = result.get(result.size() - 1);
-                if (last[1] < interval[0]) {
-                    result.add(interval);
-                } else {
-                    last[1] = Math.max(last[1], interval[1]);
-                }
-            }
-            return result.toArray(new int[0][]);
-        }
-    }
-
     public int[][] merge(int[][] intervals) {
-        if (intervals == null || intervals.length == 0) {
+        int n = intervals.length;
+        if (n < 2) {
             return intervals;
         }
-        int length = intervals.length;
-        int[] starts = new int[length];
-        int[] ends = new int[length];
-        for (int i = 0; i < length; i++) {
-            starts[i] = intervals[i][0];
-            ends[i] = intervals[i][1];
-        }
-        Arrays.sort(starts);
-        Arrays.sort(ends);
-        List<int[]> result = new ArrayList<>(length);
-        for (int i = 0, j = 0; i < length; i++) {
-            if (i == length - 1 || starts[i + 1] > ends[i]) {
-                result.add(new int[] {starts[j], ends[i]});
-                j = i + 1;
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        List<int[]> result = new ArrayList<>(n);
+        int[] last = intervals[0];
+        result.add(last);
+        for (int[] interval : intervals) {
+            if (last[1] >= interval[0]) {
+                last[1] = Math.max(last[1], interval[1]);
+            } else {
+                last = interval;
+                result.add(last);
             }
         }
         return result.toArray(new int[0][]);
--- src/test/java/leetcode/Problem56Test.java
+++ src/test/java/leetcode/Problem56Test.java
@@ -2,39 +2,39 @@ package leetcode;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class Problem56Test {
-    protected Problem56 solution = new Problem56();
+    private final Problem56 solution = new Problem56();
 
-    public static final class AlternativeTest extends Problem56Test {
-        {
-            this.solution = new Problem56.Alternative();
-        }
+    @Test
+    public void example1() {
+        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+        assert Arrays.deepEquals(solution.merge(intervals),
+            new int[][] {{1,6},{8,10},{15,18}});
     }
 
     @Test
-    public void empty() {
-        assertThat(solution.merge(new int[][] {}),
-            is(new int[][] {}));
+    public void example2() {
+        int[][] intervals = {{1,4},{4,5}};
+        assert Arrays.deepEquals(solution.merge(intervals),
+            new int[][] {{1,5}});
     }
 
     @Test
-    public void complex() {
-        assertThat(solution.merge(new int[][] {{2, 3}, {2, 2}, {3, 3}, {1, 3}, {5, 7}, {2, 2}, {4,6}}),
-            is(new int[][] {{1, 3}, {4, 7}}));
+    public void example3() {
+        int[][] intervals = {};
+        assert Arrays.deepEquals(solution.merge(intervals),
+            intervals);
     }
 
     @Test
-    public void simple() {
-        assertThat(solution.merge(new int[][] {{1, 3}, {2, 6}, {8, 10}, {15, 18}}),
-            is(new int[][] {{1, 6}, {8, 10}, {15, 18}}));
-    }
-
-    @Test
-    public void small() {
-        assertThat(solution.merge(new int[][] {{1, 4}, {4, 5}}),
-            is(new int[][] {{1, 5}}));
+    public void exampl4() {
+        int[][] intervals = {{2,3},{2,2},{3,3},{1,3},{5,7},{2,2},{4,6}};
+        assert Arrays.deepEquals(solution.merge(intervals),
+            new int[][] {{1,3},{4,7}});
     }
 }
