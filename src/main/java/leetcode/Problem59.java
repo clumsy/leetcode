83395beaae6aa30202d2f0f49aa660feba999fc3
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-22 23:12:23 -0500

    updating problem 59

--- src/main/java/leetcode/Problem59.java
+++ src/main/java/leetcode/Problem59.java
@@ -1,27 +1,19 @@
 package leetcode;
 
+@Difficulty(Level.MEDIUM)
+@Algorithms(Algorithm.SIMULATION)
 @BeatsPercent(100)
+@TimeComplexity(worst = Complexity.QUADRATIC_N)
+@SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem59 {
     public int[][] generateMatrix(int n) {
         int[][] matrix = new int[n][n];
-        int number = 1;
-        for (int k = 0; k < (n+1)/2; k++) {
-            int last = n - 1 - k;
-            if (k == last) {
-                matrix[k][k] = number++;
-                return matrix;
-            }
-            for (int i = k; i < last; i++) {
-                matrix[k][i] = number++;
-            }
-            for (int i = k; i < last; i++) {
-                matrix[i][last] = number++;
-            }
-            for (int i = last; i > k; i--) {
-                matrix[last][i] = number++;
-            }
-            for (int i = last; i > k; i--) {
-                matrix[i][k] = number++;
+        for (int k = 1, max = n*n, i = 0, j = 0, di = 0, dj = 1; k <= max; k++, i += di, j += dj) {
+            matrix[i][j] = k;
+            if (matrix[(i + di + n)%n][(j + dj + n)%n] > 0) {
+                int tmp = di;
+                di = dj;
+                dj = -tmp;
             }
         }
         return matrix;
--- src/test/java/leetcode/Problem59Test.java
+++ src/test/java/leetcode/Problem59Test.java
@@ -1,20 +1,27 @@
 package leetcode;
 
-import static org.junit.Assert.assertThat;
-
-import static org.hamcrest.core.Is.is;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class Problem59Test {
     private final Problem59 solution = new Problem59();
 
     @Test
     public void example1() {
-        assertThat(solution.generateMatrix(3),
-            is(new int[][] {
-                {1,2,3},
-                {8,9,4},
-                {7,6,5}
-            }));
+        assert Arrays.deepEquals(solution.generateMatrix(3),
+            new int[][] {
+                {1, 2, 3},
+                {8, 9, 4},
+                {7, 6, 5}
+            });
+    }
+
+    @Test
+    public void example2() {
+        assert Arrays.deepEquals(solution.generateMatrix(1),
+            new int[][] {
+                {1}
+            });
     }
 }
