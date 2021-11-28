19b5d427fc4174d3d386a2a6a27619c13b3db2de
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-27 23:09:47 -0500

    updating problem 322

--- src/main/java/leetcode/Problem322.java
+++ src/main/java/leetcode/Problem322.java
@@ -1,23 +1,25 @@
 package leetcode;
 
+import java.util.Arrays;
+
+@Difficulty(Level.MEDIUM)
+@Algorithms(Algorithm.DYNAMIC_PROGRAMMING)
 @BeatsPercent(87.94)
-@TimeComplexity(Complexity.N_BY_M) // N - amount, K - number of coins
-@SpaceComplexity(Complexity.LINEAR_N)
+@TimeComplexity(worst = Complexity.N_BY_M) // N - amount, M - number of coins
+@SpaceComplexity(worst = Complexity.LINEAR_N)
 public class Problem322 {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
+        int none = amount + 1;
+        Arrays.fill(dp, none);
         dp[0] = 0;
-        int token = amount + 1;
-        for (int i = 1; i < token; i++) {
-            dp[i] = token;
-        }
-        for (int i = 1; i < token; i++) {
+        for (int i = 1; i < none; i++) {
             for (int coin : coins) {
                 if (i >= coin) {
                     dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
                 }
             }
         }
-        return dp[amount] != token ? dp[amount] : -1;
+        return dp[amount] != none ? dp[amount] : -1;
     }
 }
--- src/test/java/leetcode/Problem322Test.java
+++ src/test/java/leetcode/Problem322Test.java
@@ -2,19 +2,18 @@ package leetcode;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
 public class Problem322Test {
     private final Problem322 solution = new Problem322();
     
     @Test
     public void example1() {
-        assertThat(solution.coinChange(new int[] {1,2,5}, 11), is(3));
+        int[] coins = {1, 2, 5};
+        assert solution.coinChange(coins, 11) == 3;
     }
     
     @Test
     public void example2() {
-        assertThat(solution.coinChange(new int[] {2}, 3), is(-1));
+        int[] coins = {2};
+        assert solution.coinChange(coins, 3) == -1;
     }
 }
