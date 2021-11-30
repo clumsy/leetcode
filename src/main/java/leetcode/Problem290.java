38067fef9a94d837d0ee26c8e6d2c1e21d4b99da
Alexander Jipa <alexander.jipa@gmail.com>
2021-11-29 22:29:25 -0500

    updating problem 290

--- src/main/java/leetcode/Problem290.java
+++ src/main/java/leetcode/Problem290.java
@@ -4,17 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+@Difficulty(Level.EASY)
 @BeatsPercent(100)
 @Algorithms(Algorithm.INDEXING)
-@TimeComplexity(Complexity.LINEAR_N)
-@SpaceComplexity(Complexity.LINEAR_N)
+@TimeComplexity(worst = Complexity.LINEAR_N)
+@SpaceComplexity(worst = Complexity.LINEAR_N)
 public class Problem290 {
     public boolean wordPattern(String pattern, String s) {
-        char[] p = pattern.toCharArray();
         String[] w = s.split(" ");
-        if (p.length != w.length) {
+        if (pattern.length() != w.length) {
             return false;
         }
+        char[] p = pattern.toCharArray();
         Map<Object, Integer> map = new HashMap<>();
         for (int i = 0; i < w.length; i++) {
             if (!Objects.equals(map.put(p[i], i), map.put(w[i], i))) {
--- src/test/java/leetcode/Problem290Test.java
+++ src/test/java/leetcode/Problem290Test.java
@@ -2,29 +2,26 @@ package leetcode;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
 public class Problem290Test {
     private final Problem290 solution = new Problem290();
     
     @Test
     public void example1() {
-        assertThat(solution.wordPattern("abba", "dog cat cat dog"), is(true));
+        assert solution.wordPattern("abba", "dog cat cat dog");
     }
     
     @Test
     public void example2() {
-        assertThat(solution.wordPattern("abba", "dog cat cat fish"), is(false));
+        assert !solution.wordPattern("abba", "dog cat cat fish");
     }
     
     @Test
     public void example3() {
-        assertThat(solution.wordPattern("aaaa", "dog cat cat dog"), is(false));
+        assert !solution.wordPattern("aaaa", "dog cat cat dog");
     }
     
     @Test
     public void example4() {
-        assertThat(solution.wordPattern("abba", "dog dog dog dog"), is(false));
+        assert !solution.wordPattern("abba", "dog dog dog dog");
     }
 }
