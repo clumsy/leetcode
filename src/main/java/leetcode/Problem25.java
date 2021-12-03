1300be9df85c5f9f86708fc1af03b78c59c2a6f9
Alexander Jipa <alexander.jipa@gmail.com>
2021-12-02 23:38:28 -0500

    updating problem 25

--- src/main/java/leetcode/Problem25.java
+++ src/main/java/leetcode/Problem25.java
@@ -6,25 +6,33 @@ package leetcode;
 @TimeComplexity(worst = Complexity.LINEAR_N)
 @SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem25 {
-    public ListNode reverseKGroup(final ListNode head, final int k) {
-        ListNode current = head;
-        int n = k;
-        while (--n > 0 && current != null) {
-            current = current.next;
-        }
-        if (current == null) {
-            return head;
-        }
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode pointer = dummy;
+        while (true) {
+            ListNode node = pointer;
+            // first check whether there are k nodes to reverse
+            for (int i = 0; i < k && node != null; i++) {
+                node = node.next;
+            }
+            if (node == null) {
+                break;
+            }
 
-        n = k;
-        ListNode newHead = head;
-        while (--n > 0) {
-            ListNode prev = newHead;
-            newHead = head.next;
-            head.next = newHead.next;
-            newHead.next = prev;
+            // now we know that we have k nodes, we will start from the first node
+            ListNode prev = null, curr = pointer.next;
+            for (int i = 0; i < k; i++) {
+                ListNode next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            ListNode tail = pointer.next;
+            tail.next = curr;
+            pointer.next = prev;
+            pointer = tail;
         }
-        head.next = reverseKGroup(head.next, k);
-        return newHead;
+        return dummy.next;
     }
 }
