da54cdfd1b3c3823936eb193db2a7b45a3d1dd7e
Alexander Jipa <alexander.jipa@gmail.com>
2021-12-06 22:14:21 -0500

    updating problem 173

--- src/main/java/leetcode/Problem173.java
+++ src/main/java/leetcode/Problem173.java
@@ -3,14 +3,15 @@ package leetcode;
 import java.util.Deque;
 import java.util.LinkedList;
 
+@Difficulty(Level.MEDIUM)
 @Algorithms(Algorithm.IN_ORDER_TRAVERSAL)
 @BeatsPercent(99.13)
-@TimeComplexity(Complexity.LINEAR_N) // where N is the number of nodes in the tree
-@SpaceComplexity(Complexity.LINEAR_K) // where K is the height of the tree
 public class Problem173 {
     class BSTIterator {
         private final Deque<TreeNode> stack = new LinkedList<>();
 
+        @TimeComplexity(worst = Complexity.LINEAR_N, average = Complexity.LINEAR_N) // where N is the number of nodes in the tree
+        @SpaceComplexity(worst = Complexity.LINEAR_K) // where K is the height of the tree
         public BSTIterator(TreeNode root) {
             push(root);
         }
@@ -22,12 +23,17 @@ public class Problem173 {
             }
         }
 
+        @TimeComplexity(worst = Complexity.LINEAR_N, average = Complexity.LINEAR_N) // where N is the number of nodes in the tree
+        @SpaceComplexity(Complexity.LINEAR_K) // where K is the height of the tree
         public int next() {
             TreeNode next = stack.pop();
             push(next.right);
             return next.val;
         }
 
+
+        @TimeComplexity(worst = Complexity.CONSTANT)
+        @SpaceComplexity(Complexity.LINEAR_K)
         public boolean hasNext() {
             return !stack.isEmpty();
         }
--- src/test/java/leetcode/Problem173Test.java
+++ src/test/java/leetcode/Problem173Test.java
@@ -3,24 +3,21 @@ package leetcode;
 import leetcode.Problem173.BSTIterator;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
 public class Problem173Test {
     private final Problem173 solution = new Problem173();
     
     @Test
     public void example1() {
-        TreeNode tree = TreeNodes.treeOf(7, 3, 15, null, null, 9, 20);
+        TreeNode tree = TreeNodes.treeOf(7,3,15,null,null,9,20);
         BSTIterator iterator = solution.new BSTIterator(tree);
-        List<Integer> result = new ArrayList<>();
-        while (iterator.hasNext()) {
-            result.add(iterator.next());
-        }
-        assertThat(result, is(Arrays.asList(3,7,9,15,20)));
+        assert iterator.next() == 3;
+        assert iterator.next() == 7;
+        assert iterator.hasNext();
+        assert iterator.next() == 9;
+        assert iterator.hasNext();
+        assert iterator.next() == 15;
+        assert iterator.hasNext();
+        assert iterator.next() == 20;
+        assert !iterator.hasNext();
     }
 }
