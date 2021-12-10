9b71de5317d6642e9c37527a6073d3670545e3fd
Alexander Jipa <alexander.jipa@gmail.com>
2021-12-09 22:34:57 -0500

    updating problem 215

--- src/main/java/leetcode/Problem215.java
+++ src/main/java/leetcode/Problem215.java
@@ -1,49 +1,24 @@
 package leetcode;
 
-import java.util.PriorityQueue;
-
+@Difficulty(Level.MEDIUM)
+@Algorithms(Algorithm.COUNTING_SORT)
+@BeatsPercent(98.59)
+@TimeComplexity(worst = Complexity.LINEAR_N)
+@SpaceComplexity(worst = Complexity.CONSTANT)
 public class Problem215 {
-    public static class Alternative extends Problem215 {
-
-        @Override
-        public int findKthLargest(int[] nums, int k) {
-            int index = kthSmallestIndex(nums, 0, nums.length - 1, nums.length - k + 1);
-            return nums[index];
-        }
-
-        private int kthSmallestIndex(int[] nums, int lo, int hi, int k) {
-            int i = lo, j = hi, pivot = nums[hi];
-            while (i < j) {
-                if (nums[i++] > pivot) {
-                    swap(nums, --i, --j);
-                }
-            }
-            swap(nums, i, hi);
-            int lessThanPivot = i - lo + 1;
-            if (lessThanPivot == k) {
-                return i;
-            }
-            if (lessThanPivot > k) {
-                return kthSmallestIndex(nums, lo, i - 1, k);
-            }
-            return kthSmallestIndex(nums, i + 1, hi, k - lessThanPivot);
-        }
-
-        private void swap(int[] nums, int i, int j) {
-            int tmp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = tmp;
-        }
-    }
+    private static final int MAX = 10000;
 
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
-        for (int i : nums) {
-            minHeap.offer(i);
-            if (minHeap.size() > k) {
-                minHeap.poll();
+        int[] count = new int[2*MAX + 1];
+        for (int num : nums) {
+            ++count[num + MAX];
+        }
+        for (int i = count.length - 1; i >= 0; --i) {
+            k -= count[i];
+            if (k <= 0) {
+                return i - MAX;
             }
         }
-        return minHeap.peek();
+        throw new RuntimeException();
     }
 }
--- src/test/java/leetcode/Problem215Test.java
+++ src/test/java/leetcode/Problem215Test.java
@@ -2,25 +2,18 @@ package leetcode;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
 public class Problem215Test {
-    protected Problem215 solution = new Problem215();
+    private final Problem215 solution = new Problem215();
 
-    public static final class AlternativeTest extends Problem215Test {
-        {
-            this.solution = new Problem215.Alternative();
-        }
+    @Test
+    public void example1() {
+        int[] nums = {3,2,1,5,6,4};
+        assert solution.findKthLargest(nums, 2) == 5;
     }
 
     @Test
-    public void simple() {
-        assertThat(solution.findKthLargest(new int[] {3, 2, 1, 5, 6, 4}, 2), is(5));
-    }
-
-    @Test
-    public void bigger() {
-        assertThat(solution.findKthLargest(new int[] {3, 2, 3, 1, 2, 4, 5, 5, 6}, 4), is(4));
+    public void example2() {
+        int[] nums = {3,2,3,1,2,4,5,5,6};
+        assert solution.findKthLargest(nums, 4) == 4;
     }
 }
