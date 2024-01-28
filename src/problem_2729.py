13bd5df29806da76dffcd99f34579cee61511501
Alexander Jipa <alexander.jipa@gmail.com>
2024-01-27 22:36:56 -0500

    problem 2799

---
+++ src/problem_2729.py
@@ -0,0 +1,53 @@
+import marimo
+
+__generated_with = "0.1.81"
+app = marimo.App()
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        [Problem 2729](https://leetcode.com/problems/check-if-the-number-is-fascinating/description)
+        """
+    )
+    return
+
+
+@app.cell
+def _():
+    class Solution:
+        def isFascinating(self, n: int) -> bool:
+            s = str(n) + str(2*n) + str(3*n)
+            res = len(s) == 9 and set(range(1, 10)) == set(int(c) for c in s)
+            return res
+    return Solution,
+
+
+@app.cell
+def _(Solution):
+    sol = Solution()
+    return sol,
+
+
+@app.cell
+def _(sol):
+    assert sol.isFascinating(192)
+    return
+
+
+@app.cell
+def _(sol):
+    assert not sol.isFascinating(100)
+    return
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return mo,
+
+
+if __name__ == "__main__":
+    app.run()
+
---
+++ src/problem_2799.py
@@ -0,0 +1,64 @@
+import marimo
+
+__generated_with = "0.1.81"
+app = marimo.App()
+
+
+@app.cell
+def __(mo):
+    mo.md("[Problem 2799](https://leetcode.com/problems/count-complete-subarrays-in-an-array/description)")
+    return
+
+
+@app.cell
+def __():
+    from typing import List
+    from collections import Counter
+    return Counter, List
+
+
+@app.cell
+def __(Counter, List):
+    class Solution:
+        def countCompleteSubarrays(self, nums: List[int]) -> int:
+            res = i = 0
+            cnt, n = len(set(nums)), len(nums)
+            cur = Counter()
+            for j in range(n):
+                cur[nums[j]] += 1
+                while len(cur) == cnt:
+                    cur[nums[i]] -= 1
+                    if cur[nums[i]] == 0:
+                        del cur[nums[i]]
+                    i += 1
+                res += i
+            return res
+    return Solution,
+
+
+@app.cell
+def __(Solution):
+    sol = Solution()
+    return sol,
+
+
+@app.cell
+def __(sol):
+    assert sol.countCompleteSubarrays([1,3,1,2,2]) == 4
+    return
+
+
+@app.cell
+def __(sol):
+    assert sol.countCompleteSubarrays([5,5,5,5]) == 10
+    return
+
+
+@app.cell
+def __():
+    import marimo as mo
+    return mo,
+
+
+if __name__ == "__main__":
+    app.run()
