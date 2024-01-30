ce123b91c7f668acce01b1498e90cb1a53ca39bd
Alexander Jipa <alexander.jipa@gmail.com>
2024-01-29 23:55:11 -0500

    marimoing problem 1002

---
+++ src/problem_1002.py
@@ -0,0 +1,62 @@
+import marimo
+
+__generated_with = "0.1.81"
+app = marimo.App()
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        [Problem 1002](https://leetcode.com/problems/find-common-characters/description)
+        """
+    )
+    return
+
+
+@app.cell
+def _():
+    from typing import List
+    from collections import Counter
+    return Counter, List
+
+
+@app.cell
+def _(Counter, List):
+    class Solution:
+        def commonChars(self, words: List[str]) -> List[str]:
+            cnt = Counter(words[0])
+            for w in words[1:]:
+                cnt &= Counter(w)
+            res = list(cnt.elements())
+            return res
+    return Solution,
+
+
+@app.cell
+def _(Solution):
+    sol = Solution()
+    return sol,
+
+
+@app.cell
+def _(sol):
+    assert sol.commonChars(["bella","label","roller"]) == ["e","l","l"]
+    return
+
+
+@app.cell
+def _(sol):
+    assert sol.commonChars(["cool","lock","cook"]) == ["c","o"]
+    return
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return mo,
+
+
+if __name__ == "__main__":
+    app.run()
+
