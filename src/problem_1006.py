fdafc2ded524c766f2609814cd6385212a592423
Alexander Jipa <alexander.jipa@gmail.com>
2024-02-02 00:04:58 -0500

    marimoing problem 1006

---
+++ src/problem_1006.py
@@ -0,0 +1,62 @@
+import marimo
+
+__generated_with = "0.1.81"
+app = marimo.App()
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        [Problem 1006](https://leetcode.com/problems/clumsy-factorial/description)
+        """
+    )
+    return
+
+
+@app.cell
+def _():
+    class Solution:
+        def clumsy(self, n: int) -> int:
+            res, sign = 0, 1
+            while n > 3:
+                res += sign * (n * (n - 1) // (n - 2)) + n - 3
+                n -= 4
+                sign = -1
+            if n > 2:
+                res += sign * (n * (n - 1) // (n - 2))
+            elif n > 1:
+                res += sign * n * (n - 1)
+            else:
+                res += sign * n
+            return res
+    return Solution,
+
+
+@app.cell
+def _(Solution):
+    sol = Solution()
+    return sol,
+
+
+@app.cell
+def _(sol):
+    assert sol.clumsy(4) == 7
+    return
+
+
+@app.cell
+def _(sol):
+    assert sol.clumsy(10) == 12
+    return
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return mo,
+
+
+if __name__ == "__main__":
+    app.run()
+
