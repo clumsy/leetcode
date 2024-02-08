96e74126f8e8b380aaf2e2d949e3fc85d8f3f456
Alexander Jipa <alexander.jipa@gmail.com>
2024-02-07 22:49:21 -0500

    problem 1017

---
+++ src/problem_1017.py
@@ -0,0 +1,58 @@
+import marimo
+
+__generated_with = "0.1.81"
+app = marimo.App()
+
+
+@app.cell
+def __(mo):
+    mo.md("[Problem 1017](https://leetcode.com/problems/convert-to-base-2/description)")
+    return
+
+
+@app.cell
+def __():
+    class Solution:
+        def baseNeg2(self, n: int) -> str:
+            res = []
+            while n != 0:
+                n, r = divmod(n, 2)
+                n *= -1
+                res.append(str(r))
+            res = "".join(res[::-1] or ["0"])
+            return res
+    return Solution,
+
+
+@app.cell
+def __(Solution):
+    sol = Solution()
+    return sol,
+
+
+@app.cell
+def __(sol):
+    assert sol.baseNeg2(2) == "110"
+    return
+
+
+@app.cell
+def __(sol):
+    assert sol.baseNeg2(3) == "111"
+    return
+
+
+@app.cell
+def __(sol):
+    assert sol.baseNeg2(4) == "100"
+    return
+
+
+@app.cell
+def __():
+    import marimo as mo
+    return mo,
+
+
+if __name__ == "__main__":
+    app.run()
