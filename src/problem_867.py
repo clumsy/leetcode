937e2e36a7d6f1839b8389bf73b819d6d2292047
Alexander Zhipa <alex.zhipa@proton.me>
2025-03-26 23:59:26 -0400

    problem 867 https://leetcode.com/problems/transpose-matrix

---
+++ src/problem_867.py
@@ -0,0 +1,8 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        m, n = len(matrix), len(matrix[0])
+        res = [[0] * m for _ in range(n)]
+        for r in range(m):
+            for c in range(n):
+                res[c][r] = matrix[r][c]
+        return res
