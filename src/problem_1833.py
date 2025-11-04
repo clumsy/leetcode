b0c4fbdd1e57a373e90c9c222427a862b9422063
Alexander Zhipa <alex.zhipa@proton.me>
2025-11-03 22:46:05 -0500

    problem 1833 https://leetcode.com/problems/maximum-ice-cream-bars

---
+++ src/problem_1833.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        res = 0
+        for i in range(len(costs)):
+            coins -= costs[i]
+            if coins < 0:
+                break
+            res += 1
+        return res
