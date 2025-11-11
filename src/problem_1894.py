ca33d4ceaeff74f8a4f85169491d4ed505414d68
Alexander Zhipa <alex.zhipa@proton.me>
2025-11-11 07:44:11 -0500

    problem 1894 https://leetcode.com/problems/find-the-student-that-will-replace-the-chalk

---
+++ src/problem_1894.py
@@ -0,0 +1,7 @@
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        for i in range(len(chalk) - 1):
+            chalk[i + 1] += chalk[i]
+        k %= chalk[-1]
+        res = bisect_right(chalk, k)
+        return res
