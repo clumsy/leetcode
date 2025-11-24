b487c142abb7e35060fa03b03267e307286d6743
Alexander Zhipa <alex.zhipa@proton.me>
2025-11-23 22:54:52 -0500

    problem 1845 https://leetcode.com/problems/seat-reservation-manager

---
+++ src/problem_1845.py
@@ -0,0 +1,9 @@
+class SeatManager:
+    def __init__(self, n: int):
+        self.heap = list(range(1, n + 1))
+
+    def reserve(self) -> int:
+        return heappop(self.heap)
+
+    def unreserve(self, seatNumber: int) -> None:
+        heappush(self.heap, seatNumber)
