a03030f50b2c2f16c27b248f4cdbea6e85acad4b
Alexander Zhipa <alex.zhipa@proton.me>
2026-01-22 23:32:58 -0500

    problem 162 https://leetcode.com/problems/find-peak-element

---
+++ src/problem_162.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        lo, hi = 0, len(nums) - 1
+        while lo < hi:
+            mi = (lo + hi) // 2
+            if nums[mi] <= nums[mi + 1]:
+                lo = mi + 1
+            else:
+                hi = mi
+        return lo
