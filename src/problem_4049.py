1f3c49491f6b06909683afaef73aad664dd1d645
Alexander Zhipa <alex.zhipa@proton.me>
2026-09-12 22:34:32 -0400

    problem 4049 https://leetcode.com/problems/count-values-with-equally-spaced-occurrences-ii

---
+++ src/problem_4049.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countSpecialIntegers(self, nums: list[int]) -> int:
+        cnt, dst, lst = defaultdict(int), defaultdict(int), defaultdict(int)
+        for i, v in enumerate(nums):
+            cnt[v] += 1
+            if cnt[v] == 2:
+                dst[v] = i - lst[v] + 1
+            elif cnt[v] > 2 and (i - lst[v] + 1) != dst[v]:
+                dst[v] = 0
+            lst[v] = i
+        res = sum(k > 2 and dst[v] > 0 for v, k in cnt.items())
+        return res
