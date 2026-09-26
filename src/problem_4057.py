80bdd558d1693528cb4ccfa1224e08ef099f4c1b
Alexander Zhipa <alex.zhipa@proton.me>
2026-09-25 23:22:52 -0400

    problem 4057 https://leetcode.com/problems/number-of-intersecting-interval-pairs-ii

---
+++ src/problem_4057.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countIntersectingIntervals(self, ints: list[list[int]]) -> int:
+        res = cur = 0
+        q = sorted(i for s, e in ints for i in ((s, +1), (e + 1, -1)))
+        for _, d in q:
+            res += cur if d > 0 else 0
+            cur += d
+        return res
